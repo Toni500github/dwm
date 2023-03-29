c6611db0c3a770b0f6aeb5b070c6fdc63c166c12
Toni500github <toni500minecraft@gmail.com>
2023-03-29 16:50:36 +0200

    forgot to do git add . (again)

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
