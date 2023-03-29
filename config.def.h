c482fb104b33631e771d5e910896a297aba35d8b
Toni500github <toni500minecraft@gmail.com>
2023-03-29 11:35:15 +0200

    adding statusbutton patch

--- config.def.h
+++ config.def.h
@@ -14,6 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char buttonbar[]       = "<O>";
 #define ICONSIZE 16
 #define ICONSPACING 5
 static const char *fonts[]          = { "monospace:size=10", "FontAwesome:style=Regular:pixelsize=15", "Iosevka:style:medium:size=12", "JetBrainsMono Nerd Font Mono:style:medium:size=12" };
@@ -133,7 +134,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkButton,		        0,		          Button1,	      spawn,		      {.v = dmenucmd } },
+  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	//{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
