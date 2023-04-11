d8b150fd262c7df3331cc3a8c817ca2578430ee0
Toni500github <toni500minecraft@gmail.com>
2023-04-11 13:26:17 +0200

    idk

--- config.def.h
+++ config.def.h
@@ -134,7 +134,7 @@ ResourcePref resources[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-nhb", normbgcolor, "-shb", selbordercolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
