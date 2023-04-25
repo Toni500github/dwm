646401ab983f7f0d271e424230792e71f83270ce
Toni500github <toni500minecraft@gmail.com>
2023-04-25 16:49:27 +0200

    chaning center floating windows patch

--- dwm.c
+++ dwm.c
@@ -1572,12 +1572,12 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
-	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
-        c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	c->sfx = c->x;
         c->sfy = c->y;
         c->sfw = c->w;
         c->sfh = c->h;
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+        c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	{
 		int format;
 		unsigned long *data, n, extra;
