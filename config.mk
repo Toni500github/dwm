d74a0b25ebba949af906fe409b3712698ab31f2c
Toni500github <toni500minecraft@gmail.com>
2023-05-10 16:18:43 +0200

    config.mk minor change

--- config.mk
+++ config.mk
@@ -28,7 +28,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-r
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -Wno-discarded-qualifiers -Wno-incompatible-pointer-types -Wno-deprecated-declarations -Wno-unused-result -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -Wno-discarded-qualifiers -Wno-incompatible-pointer-types -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
