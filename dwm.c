3100237c21216ebfe411362f1964f52c4d633d0a
Toni500github <toni500minecraft@gmail.com>
2023-08-05 19:26:54 +0200

    adding back alwayscenter patch

--- dwm.c
+++ dwm.c
@@ -1677,6 +1677,8 @@ manage(Window w, XWindowAttributes *wa)
         c->sfy = c->y;
         c->sfw = c->w;
         c->sfh = c->h;
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	{
 		int format;
 		unsigned long *data, n, extra;
